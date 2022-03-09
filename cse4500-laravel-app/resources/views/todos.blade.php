40b989e32b66832d6dd62b26c78ae096719b9adb
csusb-cv-gh <63890345+csusb-cv-gh@users.noreply.github.com>
2022-03-08 22:57:46 -0800

    thing

--- cse4500-laravel-app/resources/views/todos.blade.php
+++ cse4500-laravel-app/resources/views/todos.blade.php
@@ -1,4 +1,5 @@
 @extends('adminlte::page')
+@section('plugins.Datatables', true)
 
 @section('title', 'To Do List')
 
@@ -7,13 +8,37 @@
 @stop
 
 @section('content')
-    <p>Content here</p>
-@stop
-
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1.</td>
+          <td>Update software</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: 55%"></div></div></td>
+          <td><span class="badge bg-danger">55%</span></td>
+        </tr>
+        <tr>
+          <td>2.</td>
+          <td>Clean database</td>
+          <td><div class="progress progress-xs"><div class="progress-bar bg-warning" style="width: 70%"></div></div></td>
+          <td><span class="badge bg-warning">70%</span></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
 @stop
