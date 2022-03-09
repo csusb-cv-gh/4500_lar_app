1aebaba75ddd636ed579a9fa8e9b0a849381ce99
csusb-cv-gh <63890345+csusb-cv-gh@users.noreply.github.com>
2022-03-09 00:39:30 -0800

    Update web.php

--- cse4500-laravel-app/routes/web.php
+++ cse4500-laravel-app/routes/web.php
@@ -29,15 +29,37 @@ Route::get(' /board', function () {
     return view('board');
 });
 
-Route::get('/events-feed', function () {
+/*Route::get('/events-feed', function () {
     return '[
         {
           "title": "CSE4500 Class",
           "start": "2022-02-23T17:30:00",
           "end": "2022-02-23T18:45:00"
-        },  
+        },
+        {
+          "title": "CSE4500 Class",
+          "start": "2022-02-28T17:30:00",
+          "end": "2022-02-28T18:45:00"
+        },   
       ]';
-});
+});*/
+
+Route::get('/events-feed', function () {
+    $data = array(
+        array(
+            'Title' => "CSSSE4500 Class",
+            'Start' => "2022-02-23T17:30:00",
+            'End' => "2022-02-23-T18:45:00"
+    ));
+    Return json_encode($data);
+    });
+    
+    
+
+//Route::fallback(function(){
+//	Return view('404');
+//});
+
 
 
 Route::get('/db-test', function(){
